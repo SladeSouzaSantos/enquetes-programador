a4a8c541664a1c459c85a560d78e4802efbb0689
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2022-02-08 22:09:29 -0300

    change Authentication usacase to receive a model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -3,9 +3,15 @@ import 'package:meta/meta.dart';
 import '../entities/entities.dart';
 
 abstract class Authentication{
-  Future<AccountEntity> auth(
-      {
-        @required String email,
-        @required String password
-      });
+  Future<AccountEntity> auth(AuthenticantionParams params);
+}
+
+class AuthenticantionParams{
+  final String email;
+  final String secret;
+
+  AuthenticantionParams({
+    @required this.email,
+    @required this.secret
+  });
 }
