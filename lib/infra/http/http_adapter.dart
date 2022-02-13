ca709d88bcfaa4437c40cc1bcd0dcf6415f4a5c8
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2022-02-13 17:51:58 -0300

    move duplicated code to a private method

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -15,20 +15,23 @@ class HttpAdapter implements HttpClient{
     @required String method,
     Map body
   }) async{
-
     final headers = {
       "content-type" : "application/json",
       "accept" : "application/json"
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
-
     final response = await client.post(url, headers: headers, body: jsonBody);
 
+    return _handleResponse(response);
+
+  }
+
+  Map _handleResponse(Response response){
     if(response.statusCode == 200){
       return response.body.isEmpty ? null : jsonDecode(response.body);
     }else{
       return null;
     }
-
   }
+
 }
