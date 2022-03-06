d2a0077f8fde353303b7e2236bbac5ddad2534c9
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2022-03-06 00:48:31 -0300

    ensure StreamLoginPresenter emits null if email is valid

--- test/presetation/presenters/stream_login_presenter_test.dart
+++ test/presetation/presenters/stream_login_presenter_test.dart
@@ -42,4 +42,13 @@ void main(){
     sut.validateEmail(email);
     sut.validateEmail(email);
   });
+
+  test("Should emit null if validation succeds", (){
+
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.isFormValidStream.listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
 }
