679e6e0e263de6dbc5beaf1325faade7e1b007b5
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2022-05-16 20:03:01 -0300

    move RequiredFieldValidation to its own file

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -1,20 +1,6 @@
+import 'package:enquetes_programadores/validation/validators/validators.dart';
+
 import 'package:test/test.dart';
-
-abstract class FieldValidation{
-  String get field;
-  String validate(String value);
-}
-
-class RequiredFieldValidation implements FieldValidation{
-  final String field;
-
-  RequiredFieldValidation(this.field);
-
-  String validate(String value){
-    return value?.isNotEmpty == true ? null : "Campo obrigatório.";
-  }
-
-}
 
 void main() {
   RequiredFieldValidation sut;
