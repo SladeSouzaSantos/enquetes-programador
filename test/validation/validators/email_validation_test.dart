51d32d93cbdda48b1f938becca70f40d0da80fa5
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2022-05-16 22:47:56 -0300

    ensure EmailValidation returns error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -8,7 +8,9 @@ class EmailValidation implements FieldValidation{
   EmailValidation(this.field);
 
   String validate(String value){
-    return null;
+    final regex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : "Campo inválido.";
   }
 }
 
@@ -38,5 +40,12 @@ void main() {
     final error = sut.validate("pedro_eng.petroleo@hotmail.com");
 
     expect(error, null);
+  });
+
+  test("Should return null if email is invalid", () {
+
+    final error = sut.validate("pedro_eng.petroleo");
+
+    expect(error, "Campo inválido.");
   });
 }
