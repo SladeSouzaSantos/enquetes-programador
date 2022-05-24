06fd7dc2a6bae88b38d86eb79f16b486b7fe0209
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2022-05-24 16:15:17 -0300

    move production code ro their own files

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,29 +1,10 @@
+import 'package:enquetes_programadores/data/cache/cache.dart';
+import 'package:enquetes_programadores/data/usecases/usecases.dart';
 import 'package:enquetes_programadores/domain/entities/entities.dart';
 import 'package:enquetes_programadores/domain/helpers/domain_error.dart';
-import 'package:enquetes_programadores/domain/usecases/usecases.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
-
-class LocalSaveCurrentAccount implements SaveCurrentAccount{
-  final SaveSecureCacheStorage saveSecureCacheStorage;
-
-  LocalSaveCurrentAccount({@required this.saveSecureCacheStorage});
-
-  Future<void> save(AccountEntity account) async{
-    try {
-      await saveSecureCacheStorage.saveSecure(
-          key: "token", value: account.token);
-    }catch(error){
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class SaveSecureCacheStorage{
-  Future<void> saveSecure({@required String key, @required String value});
-}
 
 class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage{}
 
