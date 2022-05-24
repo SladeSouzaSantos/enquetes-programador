41b859a6004746d53fa97e2b0fd12d3f5087392c
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2022-05-23 21:13:13 -0300

    add ValidationBuilder

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,3 +1,4 @@
+import '../../../../main/builders/builders.dart';
 import '../../../../validation/protocols/protocols.dart';
 import '../../../../presetation/protocols/protocols.dart';
 import '../../../../validation/validators/validators.dart';
@@ -9,8 +10,7 @@ Validation makeLoginValidation(){
 
 List<FieldValidation> makeLoginValidations(){
   return [
-    RequiredFieldValidation("email"),
-    EmailValidation("email"),
-    RequiredFieldValidation("password"),
+    ...ValidationBuilder.field("email").required().email().build(),
+    ...ValidationBuilder.field("password").required().build()
   ];
 }
