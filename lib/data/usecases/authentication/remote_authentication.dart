abb8f5e4dab4229057b4542a6267bccf69f2fea0
SladeSouzaSantos <greencity.sustentavel@gmail.com>
2022-05-24 13:44:57 -0300

    organize folders

--- lib/data/usecases/authentication/remote_authentication.dart
+++ lib/data/usecases/authentication/remote_authentication.dart
@@ -1,9 +1,9 @@
 import 'package:meta/meta.dart';
 
-import '../http/http.dart';
-import '../../domain/entities/entities.dart';
-import '../../domain/helpers/helpers.dart';
-import '../../domain/usecases/usecases.dart';
+import '../../http/http.dart';
+import '../../../domain/entities/entities.dart';
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
 
 class RemoteAuthentication implements Authentication{
   final HttpClient httpClient;
